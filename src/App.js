b7a2beb367ad54cfa52406995d14f87223290f3d
Hugh Winchester <hugh.winchester@gmail.com>
2021-10-09 21:21:12 +0100

    Basic layout of buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import react, { useState } from "react";
-import { Button, Row, Coulumn, Container, Col } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import {
+  Button,
+  Row,
+  Container,
+  ButtonGroup,
+  ThemeProvider,
+} from "react-bootstrap";
 
 import CalculatorScreen from "./components/CalculatorScreen";
 
@@ -10,10 +15,52 @@ function App() {
   const numberTwo = useState("");
 
   return (
-    <div className="App" class="calculator">
-      <Container fluid>
-        <CalculatorScreen />
-      </Container>
+    <div className="App calculator">
+      <ThemeProvider prefixes={{ btn: "custom-btn " }}>
+        <Container fluid>
+          <CalculatorScreen />
+          <Row>
+            <ButtonGroup>
+              <Button className="custom-btn">(</Button>
+              <Button className="custom-btn">)</Button>
+              <Button className="custom-btn func-button">CE</Button>
+              <Button className="custom-btn func-button">AC</Button>
+            </ButtonGroup>
+          </Row>
+          <Row>
+            <ButtonGroup>
+              <Button className="custom-btn">7</Button>
+              <Button className="custom-btn">8</Button>
+              <Button className="custom-btn">9</Button>
+              <Button className="custom-btn func-button">/</Button>
+            </ButtonGroup>
+          </Row>
+          <Row>
+            <ButtonGroup>
+              <Button className="custom-btn">4</Button>
+              <Button className="custom-btn">5</Button>
+              <Button className="custom-btn">6</Button>
+              <Button className="custom-btn func-button">x</Button>
+            </ButtonGroup>
+          </Row>
+          <Row>
+            <ButtonGroup>
+              <Button className="custom-btn">1</Button>
+              <Button className="custom-btn">2</Button>
+              <Button className="custom-btn">3</Button>
+              <Button className="custom-btn func-button">-</Button>
+            </ButtonGroup>
+          </Row>
+          <Row>
+            <ButtonGroup>
+              <Button className="custom-btn">0</Button>
+              <Button className="custom-btn func-button">.</Button>
+              <Button className="custom-btn func-button">=</Button>
+              <Button className="custom-btn func-button">+</Button>
+            </ButtonGroup>
+          </Row>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
